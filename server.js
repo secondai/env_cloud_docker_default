c856410c784623a4d8b98002f48ba26b790f989d
Nicholas Reed <nicholas.a.reed@gmail.com>
2019-02-14 12:54:06 -0800

    .

--- server.js
+++ server.js
@@ -1,44 +1,18 @@
 const express = require('express');
 const app = express();
 
-var pgtools = require('pgtools');
+const pg = require('pg');
+const pool = new pg.Pool({
+	user: 'postgres',
+	password: 'postgres',
+	host: '127.0.0.1',
+	database: 'second-example',
+	port: '5432'
+});
 
-// const pg = require('pg');
-// const pool = new pg.Pool({
-// 	user: 'sysadmin',
-// 	host: '127.0.0.1',
-// 	database: 'mywebstore',
-// 	password: '',
-// 	port: '5432'
-// });
-
-// pool.query("SELECT NOW()", (err, res) => {
-// 	console.log('Select Now result:', err, res);
-// 	pool.end();
-// });
-
-
-const config = {
-  user: 'postgres',
-  password: '',
-  port: 5432,
-  host: 'localhost'
-}
-
-pgtools.createdb(config, 'test-db', function (err, res) {
-  if (err) {
-    console.error(err);
-    process.exit(-1);
-  }
-  console.log('Created:', res);
-
-  pgtools.dropdb(config, 'test-db', function (err, res) {
-    if (err) {
-      console.error(err);
-      process.exit(-1);
-    }
-    console.log('Dropped:', res);
-  });
+pool.query("SELECT NOW()", (err, res) => {
+	console.log('Select Now result:', err, res);
+	pool.end();
 });
 
 app.get('/', (req, res) => {
