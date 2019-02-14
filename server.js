f4b321f44102bbcbae8aa3f3f78c4fdc46a03e87
Nicholas Reed <nicholas.a.reed@gmail.com>
2019-02-14 11:46:08 -0800

    8080

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, Dokku!');
+  res.send('Hello, Test1!');
 });
 
-app.listen(5000, () => console.log('Listening on port 5000'));
+app.listen(8080, () => console.log('Listening on port 8080'));
