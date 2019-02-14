25be1f6a549a9744c8168e9d8ab4be401db53fe4
Nicholas Reed <nicholas.a.reed@gmail.com>
2019-02-14 14:31:06 -0800

    .

--- install.sh
+++ install.sh
@@ -6,13 +6,20 @@
 { # this ensures the entire script is downloaded #
 
 # expect to be root!!
+
+# git second repo 
 cd ~
 git clone https://github.com/secondai/env_cloud_docker_default.git second
 
-cd /root
+# setup hooks for updating via git push 
+cd ~
 git init --bare second.git
 cp second/docker-config/git-hooks/post-receive second.git/hooks/post-receive
-chmod +x second.git/hooks/post-receive
+chmod +x second.git/hooks/post-receive 
+
+# start containers 
+cd ~/second
+docker-compose up -d 
 
 
 } # this ensures the entire script is downloaded #
