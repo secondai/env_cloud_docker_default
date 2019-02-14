bb1c1f086e9b5aa64ec348eb3e04c41b31847d29
Nicholas Reed <nicholas.a.reed@gmail.com>
2019-02-14 12:12:24 -0800

    .

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, Test1!');
+  res.send('Hello, Test2!');
 });
 
 app.listen(8080, () => console.log('Listening on port 8080'));
