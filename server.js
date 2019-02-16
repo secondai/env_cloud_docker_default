23a187c7a33e73f9f476c792a5b96a3aea414827
Nicholas Reed <nicholas.a.reed@gmail.com>
2019-02-16 12:31:25 -0800

    .

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ pool.query("SELECT NOW()", (err, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello, Test12!');
+  res.send('Hello, Test13!');
 });
 
 app.listen(8080, () => console.log('Listening on port 8080'));
